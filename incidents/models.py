aafbf832a2f2a5e94b6577b8508751b8e80362d7
MukeshP1234 <mukeshpm.cd24@rvce.edu.in>
2026-04-24 23:05:41 +0530

    Update models.py

--- incidents/models.py
+++ incidents/models.py
@@ -1,6 +1,5 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime
-from datetime import datetime
-from .database import Base
+from sqlalchemy import Column, Integer, String, Float
+from database import Base
 
 class Incident(Base):
     __tablename__ = "incidents"
@@ -10,13 +9,3 @@ class Incident(Base):
     latitude = Column(Float)
     longitude = Column(Float)
     status = Column(String, default="active")
-    created_at = Column(DateTime, default=datetime.utcnow)
-
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    latitude = Column(Float)
-    longitude = Column(Float)
